10d440f42900094dedd55caca02767f564a59889
Matheus7p <matheus.costa.7p@gmail.com>
2024-04-30 21:18:18 -0300

    Entering data into API

--- src/components/form/Select/Select.jsx
+++ src/components/form/Select/Select.jsx
@@ -4,13 +4,13 @@ export default function Select({ text, name, options, handleOnChange, value  }) 
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
                 <option>Selecione uma opção</option>
-                {options.map((option) => (
+
+                {options && Array.isArray(options) && options.map((option) => (
                     <option 
-                    value={option.id} 
-                    key={option.id}
-                    >
+                        value={option.id} 
+                        key={option.id}>
                         {option.name}
                     </option>
                 ))}
--- src/components/project/ProjectForm/ProjectForm.jsx
+++ src/components/project/ProjectForm/ProjectForm.jsx
@@ -5,9 +5,10 @@ import SubmitButton from '../../form/Submit/SubmitButton'
 import { useEffect, useState } from 'react'
 
 
-export default function ProjectForm( { btnText }) {
+export default function ProjectForm( { handleSubmit, btnText, projectData    }) {
 
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || [])
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -23,24 +24,55 @@ export default function ProjectForm( { btnText }) {
     .catch((err) => console.log(err))
     }, [])
 
-    
+
+    const submit = (e) =>{
+        e.preventDefault()
+       // console.log(project)
+         handleSubmit(project)
+    } 
+
+    function handleChange(e) {
+        setProject({...project, [e.target.name] : e.target.value})
+    }
+
+    function handleCategory(e) {
+        setProject({
+            ...project, 
+            category: {
+             id: e.target.value,
+             name: e.target.options[e.target.selectedIndex].text,
+        },
+    })
+    }
+
+
     return (
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
 
             <Input
                 type='text'
                 text="Nome do Projeto"
                 name="name"
                 placeholder='Insira o nome do Projeto'
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''}
             />
               <Input
                 type='number'
                 text="Orçamento do Projeto"
                 name="budget"
                 placeholder='Insira o Orçamento do Projeto'
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget : ''}
             />
 
-            <Select name='category_id' text='Selecione a Categoria' options={categories}/>
+            <Select 
+            name='category_id' 
+            text='Selecione a Categoria' 
+            options={categories}
+            value={project.category ? project.category.id : ''}
+            handleOnChange={handleCategory}
+            />
             
             <SubmitButton text={btnText}/>
         </form>
